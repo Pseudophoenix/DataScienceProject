770cd5ccdf08b3e694e2e5a134cfe1340b4e28de
AlokGRetail <alok.sg@gretail.com>
2025-08-15 21:42:20 +0530

    ZIP handled

--- src/datascience/utils/common.py
+++ src/datascience/utils/common.py
@@ -24,10 +24,11 @@ def read_yaml(path_to_yaml)->ConfigBox:
         with open(path_to_yaml) as yaml_file:
             content=yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
-        return e
+        raise e
     
 @ensure_annotations
 def create_directories(path_to_directories:list, verbose=True):
