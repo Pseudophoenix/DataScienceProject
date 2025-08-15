2de27149e0b0f9e4c21175cf05c2e77327f70bbd
AlokGRetail <alok.sg@gretail.com>
2025-08-15 18:30:53 +0530

    template added for structured project

--- template.py
+++ template.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
 import logging
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+
 project_name="datascience"
-list_of_files={
+list_of_files=[
     ".github/workflows/.gtikeep",
     f"scr/{project_name}/__init__.py",# __init__.py to allow it to be used anywhere 
     f"src/{project_name}/components/__init__.py",
@@ -17,5 +19,23 @@ list_of_files={
     "config/config.yaml",
     "params.yaml",
     "schema.yaml",
-    "main.py"
-}
+    "main.py",
+    "Dockerfile",
+    "setup.py",
+    "research/research.ipynb",
+    "templates/index.html"
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True) # exist_ok=True means don't create if the file already exists
+        logging.info(f"Creating directory {filedir} for the file : {filename}")
+    if not os.path.exists(filepath) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w")as f:
+            pass
+            logging.info(f"Creating empty file {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
