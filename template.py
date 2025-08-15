bca8e77277b217379357ba37ed59827d493cd03b
AlokGRetail <alok.sg@gretail.com>
2025-08-15 18:24:38 +0530

    template added for structured project

--- template.py
+++ template.py
@@ -6,5 +6,16 @@ list_of_files={
     ".github/workflows/.gtikeep",
     f"scr/{project_name}/__init__.py",# __init__.py to allow it to be used anywhere 
     f"src/{project_name}/components/__init__.py",
-    
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/utils/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py"
 }
