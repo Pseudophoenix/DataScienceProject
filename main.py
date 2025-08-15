0acef939bbcd11347a48608572d0410732f75ed5
AlokGRetail <alok.sg@gretail.com>
2025-08-15 19:01:41 +0530

    template added for structured project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from datascienceproject!")
+# def main():
+#     print("Hello from datascienceproject!")
+# if __name__ == "__main__":
+#     main()
 
-
-if __name__ == "__main__":
-    main()
+from src.datascience import logger
+logger.info('Welcome to our custom logging data science')
