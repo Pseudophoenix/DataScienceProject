eb941a9b0cd9494be41ac74c76618f399c351f06
AlokGRetail <alok.sg@gretail.com>
2025-08-15 20:33:00 +0530

    Read Yaml added with exception handling

--- src/datascience/utils/common.py
+++ src/datascience/utils/common.py
@@ -28,3 +28,63 @@ def read_yaml(path_to_yaml)->ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         return e
+    
+@ensure_annotations
+def create_directories(path_to_directories:list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories(list): list of path of directories
+        ignore_log(bool, optional): ignore if multiple dirs is to be created. Defaults to 
+    """
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"Created directory at: {path}")
+@ensure_annotations
+def save_json(path:Path, data:dict):
+    """save json data
+
+    Args:
+        path(Path): path to json file
+        data(dict): data to be saved in json file
+    """
+    with open(path,"w") as f:
+        json.dump(data,f,indent=4)
+    logger.info(f"Json file saved at:{path}")
+@ensure_annotations
+def load_json(path:Path)->ConfigBox:
+    """load json files data
+    Args:
+        path(Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"Json file loaded successfully from:{path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data:Any, path:Path):
+    """save binary file
+    Args:
+        data(Any):data to be saved as binary
+        path(Path):path to binary file
+    """
+    joblib.dump(value=data,filename=path)
+    logger.info(f"Binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path:Path)->Any:
+    """load binary data
+
+    Args:
+        path(Path):path to binary file
+    Returns:
+        Any: object stored in the file
+    """
+    data=joblib.load(path)
+    logger.info(f"Binary file loaded from: {path}")
+    return data
